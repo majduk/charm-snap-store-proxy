7c4d78a600e89556ddbd141f26a72e644ef0ba4a
Michal Ajduk <michal.ajduk@canonical.com>
2020-10-05 15:55:32 +0200

    Additional tests

--- src/charm.py
+++ src/charm.py
@@ -9,10 +9,7 @@ from ops.main import main
 from ops.framework import StoredState
 from ops.model import ActiveStatus, BlockedStatus
 
-from subprocess import (
-    CalledProcessError,
-    check_output
-)
+import subprocess
 
 logger = logging.getLogger(__name__)
 
@@ -32,16 +29,16 @@ class SnapStoreProxyCharm(CharmBase):
                                  )
 
     def _on_install(self, _):
-        check_output(["snap", "install", "snap-store-proxy"])
-        check_output(["apt", "install", "-y", "postgresql-client-10"])
+        subprocess.check_output(["snap", "install", "snap-store-proxy"])
+        subprocess.check_output(["apt", "install", "-y", "postgresql-client-10"])
 
     def _on_update_status(self, _):
         if not self._stored.db_connected:
             self.model.unit.status = BlockedStatus("Database not connected")
             return
         try:
-            status = check_output(["snap-proxy", "status"])
-        except CalledProcessError as e:
+            status = subprocess.check_output(["snap-proxy", "status"])
+        except subprocess.CalledProcessError as e:
             status = e.output
         logger.info("Status: {}".format(status))
         srv_down = []
@@ -68,13 +65,14 @@ class SnapStoreProxyCharm(CharmBase):
             self.model.unit.status = BlockedStatus("Database relation not ready")
             return
         db_url = "postgresql://{user}:{password}@{host}:{port}/{database}".format(**data)
-        check_output(["psql", "-x", db_url, "-c", "CREATE EXTENSION \"btree_gist\""])
-        check_output(["snap-proxy", "config", "proxy.db.connection={}".format(db_url)])
+        subprocess.check_output(["psql", "-x", db_url, "-c", "CREATE EXTENSION \"btree_gist\""])
+        subprocess.check_output(["snap-proxy", "config", "proxy.db.connection={}".format(db_url)])
         self._stored.db_connected = True
 
     def _on_config_changed(self, _):
         for key in self.model.config:
-            if self.model.config[key] != self._stored.config[key]:
+            if key not in self._stored.config or \
+               self.model.config[key] != self._stored.config[key]:
                 value = self.model.config[key]
                 logger.info("Setting {} to: {}".format(key, value))
                 handle = getattr(self, "_set_{}".format(key.replace("-", "_")))
@@ -82,7 +80,7 @@ class SnapStoreProxyCharm(CharmBase):
                 self._stored.config[key] = value
 
     def _set_proxy_domain(self, domain):
-        check_output(["snap-proxy", "config", "proxy.domain={}".format(domain)])
+        subprocess.check_output(["snap-proxy", "config", "proxy.domain={}".format(domain)])
 
 
 if __name__ == "__main__":
--- tests/test_charm.py
+++ tests/test_charm.py
@@ -2,35 +2,69 @@
 # See LICENSE file for licensing details.
 
 import unittest
-from unittest.mock import Mock
+from unittest.mock import (
+    Mock,
+    patch,
+    call
+)
+from uuid import uuid4
+import random
 
 from ops.testing import Harness
 from charm import SnapStoreProxyCharm
 
 
 class TestCharm(unittest.TestCase):
+
+    def default_config(self):
+        return {
+            'proxy-domain': uuid4(),
+        }
+
+    @patch('subprocess.check_output')
+    def test_install(self, mock_subproc):
+        process_mock = Mock()
+        mock_subproc.return_value = process_mock
+        harness = Harness(SnapStoreProxyCharm)
+        harness.begin()
+        action_event = Mock()
+        harness.charm._on_install(action_event)
+        self.assertTrue(mock_subproc.called)
+        assert mock_subproc.call_args_list[0] == call(["snap", "install", "snap-store-proxy"])
+        assert mock_subproc.call_args_list[1] == call(["apt", "install", "-y",
+                                                       "postgresql-client-10"])
+
     def test_config_changed(self):
         harness = Harness(SnapStoreProxyCharm)
-        # from 0.8 you should also do:
-        # self.addCleanup(harness.cleanup)
+        self.addCleanup(harness.cleanup)
         harness.begin()
-        self.assertEqual(list(harness.charm._stored.things), [])
-        harness.update_config({"thing": "foo"})
-        self.assertEqual(list(harness.charm._stored.things), ["foo"])
+        harness.charm._set_proxy_domain = Mock()
+        default_config = self.default_config()
+        self.assertEqual(harness.charm._stored.config, {})
+        harness.update_config(default_config)
+        self.assertEqual(harness.charm._stored.config, default_config)
+        self.assertTrue(harness.charm._set_proxy_domain.called)
 
-    def test_action(self):
+    @patch('subprocess.check_output')
+    def test_db_relation_changed(self, mock_subproc):
         harness = Harness(SnapStoreProxyCharm)
+        self.addCleanup(harness.cleanup)
         harness.begin()
-        # the harness doesn't (yet!) help much with actions themselves
-        action_event = Mock(params={"fail": ""})
-        harness.charm._on_fortune_action(action_event)
-
-        self.assertTrue(action_event.set_results.called)
-
-    def test_action_fail(self):
-        harness = Harness(SnapStoreProxyCharm)
-        harness.begin()
-        action_event = Mock(params={"fail": "fail this"})
-        harness.charm._on_fortune_action(action_event)
-
-        self.assertEqual(action_event.fail.call_args, [("fail this",)])
+        app_name = str(uuid4())
+        peer = "{}/{}".format(app_name, random.randint(0, 100))
+        relation_id = harness.add_relation('db', 'pgsql')
+        harness.add_relation_unit(relation_id, peer)
+        relation_data = {
+            'user': str(uuid4()),
+            'password': str(uuid4()),
+            'host': str(uuid4()),
+            'port': str(uuid4()),
+            'database': str(uuid4()),
+        }
+        db_url = "postgresql://{user}:{password}@{host}:{port}/{database}".format(**relation_data)
+        harness.update_relation_data(relation_id, peer, relation_data)
+        assert harness.get_relation_data(relation_id, peer) == relation_data
+        assert mock_subproc.call_args_list[0] == call(['psql', '-x', db_url, '-c',
+                                                       'CREATE EXTENSION "btree_gist"'])
+        assert mock_subproc.call_args_list[1] == call(["snap-proxy", "config",
+                                                       "proxy.db.connection={}".format(db_url)])
